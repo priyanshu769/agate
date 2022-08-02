3641542c10ff75f9cbd48ec82f7e8afc4e27180a
Priyanshu <prynsu@yahoo.com>
2022-08-03 01:52:44 +0530

    Refactor: Removed unused code

--- src/Utils/ToastReducer.js
+++ src/Utils/ToastReducer.js
@@ -4,7 +4,6 @@ export const InitialToast = {
   }
   
   export const ToastReducer = (state, action) => {
-    console.log("toast triggered", state)
     switch (action.TYPE) {
       case 'set_Toast':
         return { ...state, showToast: action.PAYLOAD.showToast, toastMessage: action.PAYLOAD.toastMessage }
